9994838e449f1171632234a080c08b85574a8fd2
Javier Henao <javher@MacBook-Air-de-Javier.local>
2026-01-09 05:11:45 -0500

    uso de modelos y django admin

--- apps/profiles/models.py
+++ apps/profiles/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from .models.user import User
+from .models.instructor import InstructorProfile
+
 
 # Create your models here.
+
---
+++ apps/profiles/models/instructor.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.conf import settings
+
+
+class InstructorProfile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+    bio = models.TextField(blank=True)
+    photo = models.URLField(blank=True)
+    website = models.URLField(blank=True)
+    linkedin_url = models.URLField(blank=True)
+    social_network = models.URLField(blank=True)
+    
+    def __str__(self):
+        return f"Instructor: {self.user.get_full_name or self.user.username}"
